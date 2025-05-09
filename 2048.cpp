12007e09081d66efab0429918ce93f39e2ea0980
szxzip <szxzip@example.com>
2025-05-09 22:59:16 +0800

    Initial commit

--- 2048.cpp
+++ 2048.cpp
@@ -1,16 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "head.h"
-#include <iostream>
+#include "head"
 using namespace std;
 int main()
 {
-	int board[row][col];
-	for (int i = 0;i < row;i++)
-	{
-		for (int j = 0;j < col;j++)
-		{
-			board[i][j] = 0;
-		}
-	}
-	return 0;
+    int board[row][col];
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            board[i][j] = 0;
+        }
+    }
+    return 0;
 }
--- head.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <math.h>
-#include <string.h>
-const int row = 4;//4行
-const int col = 4;//4列
-int score = 0;//总得分
-
-
-

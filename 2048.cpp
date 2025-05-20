78bc42ef1e195644045b70fb67a11635860dc4d8
wosikeaixiaonanniang <3146715671@qq.com>
2025-05-20 17:38:18 +0800

    Add files via upload

--- 2048.cpp
+++ 2048.cpp
@@ -1,19 +1,15 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include "head.h"
 #include <iostream>
-#include <string.h>
-using namespace std;
 
+using namespace std;
 int main()
 {
 
 	initgraph(720, 900);
 	setbkcolor(RGB(254, 246, 238));
 	cleardevice();
-
 	startmenu();
-	
-
 	getchar();
 	return 0;
 }
--- head.h
+++ head.h
@@ -2,14 +2,14 @@
 #include <math.h>
 #include <string.h>
 #include <easyx.h>
-const int row = 4;//4脨脨
-const int col = 4;//4脕脨
-const int score = 0;//脳脺碌脙路脰
+const int row = 4;//4行
+const int col = 4;//4列
+const int score = 0;//总得分
 void thirdmenu();
 bool button(int x1, int y1, int x2, int y2, int msgx, int msgy);
 void gamemenu(int board[4][4]);
 void givecolor(int num, int x, int y);
-void startbuttonshow(int y, const char str[]);
+void startbuttonshow(int y, const char str[],int a,int b,int c);
 void startmenu();
 
 

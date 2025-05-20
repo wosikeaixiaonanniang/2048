47ca88c4bc4b5862b6b92b5cf2a9f5498abe7c0f
wosikeaixiaonanniang <3146715671@qq.com>
2025-05-20 16:45:21 +0800

    Update head.h

--- head.h
+++ head.h
@@ -4,7 +4,7 @@
 #include <easyx.h>
 const int row = 4;//4ÐÐ
 const int col = 4;//4ÁÐ
-int score = 0;//×ÜµÃ·Ö
+const int score = 0;//×ÜµÃ·Ö
 void thirdmenu();
 bool button(int x1, int y1, int x2, int y2, int msgx, int msgy);
 void gamemenu(int board[4][4]);

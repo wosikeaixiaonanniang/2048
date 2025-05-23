2334c9e771c9766301443343c80bba4c9f416f5a
wosikeaixiaonanniang <3146715671@qq.com>
2025-05-23 15:42:38 +0800

    Add files via upload

--- menu.cpp
+++ menu.cpp
@@ -7,28 +7,28 @@ void gameover(int score, int step)
 
 	setfillcolor(RGB(189, 172, 162));
 	fillrectangle(150, 300, 720 - 150, 300 + 300);
-	settextstyle(30, 0, "å¾®è½¯é›…é»‘");
+	settextstyle(30, 0, "Î¢ÈíÑÅºÚ");
 
 	char scoreStr[20];
 	sprintf(scoreStr, "%d", score);
 	int hspace = (100 - textwidth(scoreStr)) / 2;
 	int vspace = (50 - textheight(scoreStr)) / 2;
 	outtextxy(350 + hspace, 320 + vspace, scoreStr);
-	hspace = (100 - textwidth("æ‰€å¾—åˆ†æ•°")) / 2;
-	vspace = (50 - textheight("æ‰€å¾—åˆ†æ•°")) / 2;
-	outtextxy(225 + hspace, 320 + vspace, "æ‰€å¾—åˆ†æ•°");
+	hspace = (100 - textwidth("ËùµÃ·ÖÊı")) / 2;
+	vspace = (50 - textheight("ËùµÃ·ÖÊı")) / 2;
+	outtextxy(225 + hspace, 320 + vspace, "ËùµÃ·ÖÊı");
 
-	hspace = (100 - textwidth("æ‰€ç”¨æ­¥æ•°")) / 2;
-	vspace = (50 - textheight("æ‰€ç”¨æ­¥æ•°")) / 2;
-	outtextxy(225 + hspace, 370 + vspace, "æ‰€ç”¨æ­¥æ•°");
+	hspace = (100 - textwidth("ËùÓÃ²½Êı")) / 2;
+	vspace = (50 - textheight("ËùÓÃ²½Êı")) / 2;
+	outtextxy(225 + hspace, 370 + vspace, "ËùÓÃ²½Êı");
 	char stepStr[20];
 	sprintf(stepStr, "%d", step);
 	hspace = (100 - textwidth(stepStr)) / 2;
 	vspace = (50 - textheight(stepStr)) / 2;
 	outtextxy(350 + hspace, 370 + vspace, stepStr);
-	hspace = (100 - textwidth("æ‰€ç”¨æ—¶é—´")) / 2;
-	vspace = (50 - textheight("æ‰€ç”¨æ—¶é—´")) / 2;
-	outtextxy(225 + hspace, 420 + vspace, "æ‰€ç”¨æ—¶é—´");
+	hspace = (100 - textwidth("ËùÓÃÊ±¼ä")) / 2;
+	vspace = (50 - textheight("ËùÓÃÊ±¼ä")) / 2;
+	outtextxy(225 + hspace, 420 + vspace, "ËùÓÃÊ±¼ä");
 	hspace = (100 - textwidth("00:00:00")) / 2;
 	vspace = (50 - textheight("00:00:00")) / 2;
 	outtextxy(350 + hspace, 420 + vspace, "00:00:00");
@@ -40,19 +40,19 @@ void gameover(int score, int step)
 		{
 			setfillcolor(RGB(125, 125, 125));
 			solidrectangle(250, 500, 720 - 250, 500 + 50);
-			settextstyle(40, 0, "å¾®è½¯é›…é»‘");
-			int hspace = (220 - textwidth("ç¡®  è®¤")) / 2;
-			int vspace = (50 - textheight("ç¡®  è®¤")) / 2;
-			outtextxy(250 + hspace, 500 + vspace, "ç¡®  è®¤");
+			settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
+			int hspace = (220 - textwidth("È·  ÈÏ")) / 2;
+			int vspace = (50 - textheight("È·  ÈÏ")) / 2;
+			outtextxy(250 + hspace, 500 + vspace, "È·  ÈÏ");
 		}
 		else
 		{
 			setfillcolor(RGB(209, 191, 182));
 			solidrectangle(250, 500, 720 - 250, 500 + 50);
-			settextstyle(40, 0, "å¾®è½¯é›…é»‘");
-			int hspace = (220 - textwidth("ç¡®  è®¤")) / 2;
-			int vspace = (50 - textheight("ç¡®  è®¤")) / 2;
-			outtextxy(250 + hspace, 500 + vspace, "ç¡®  è®¤");
+			settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
+			int hspace = (220 - textwidth("È·  ÈÏ")) / 2;
+			int vspace = (50 - textheight("È·  ÈÏ")) / 2;
+			outtextxy(250 + hspace, 500 + vspace, "È·  ÈÏ");
 		}
 		EndBatchDraw();
 
@@ -61,7 +61,7 @@ void gameover(int score, int step)
 			switch (msg.message)
 			{
 			case WM_LBUTTONDOWN:
-				if (button(250, 500, 250 + 220, 500 + 50, msg.x, msg.y))//ç¡®è®¤
+				if (button(250, 500, 250 + 220, 500 + 50, msg.x, msg.y))//È·ÈÏ
 				{
 					startmenu();
 				}
@@ -104,11 +104,11 @@ void givecolor(int num, int x, int y)
 	char str[10];
 	if (num != 0)
 	{
-		sprintf(str, "%d", num);  // æ•°å­—è½¬å­—ç¬¦ä¸²
+		sprintf(str, "%d", num);  // Êı×Ö×ª×Ö·û´®
 	}
 	else
 	{
-		str[0] = '\0';  // 0 æ—¶ä¸æ˜¾ç¤ºæ–‡å­—
+		str[0] = '\0';  // 0 Ê±²»ÏÔÊ¾ÎÄ×Ö
 	}
 	switch (num)
 	{
@@ -180,7 +180,7 @@ void givecolor(int num, int x, int y)
 
 	if (num != 0)
 	{
-		settextstyle(size, 0, "å¾®è½¯é›…é»‘");
+		settextstyle(size, 0, "Î¢ÈíÑÅºÚ");
 		setbkmode(TRANSPARENT);
 		hspace = (100 - textwidth(str)) / 2;
 		vspace = (100 - textheight(str)) / 2;
@@ -192,11 +192,58 @@ void givecolor(int num, int x, int y)
 }
 void updatedisplay(int score, int board[4][4])
 {
-	settextstyle(40, 0, "å¾®è½¯é›…é»‘");
+	int bestscore = 0;
+	BeginBatchDraw();
+	//±³¾°ºÍÆåÅÌ
+	setbkcolor(RGB(254, 246, 238));
+	cleardevice();
+	setfillcolor(RGB(189, 172, 162));
+	solidroundrect(100, 320, 100 + 520, 320 + 520, 50, 50);
+	//2048ÎÄ×Ö
+	settextstyle(100, 0, "Î¢ÈíÑÅºÚ");
 	setbkmode(TRANSPARENT);
 	settextcolor(DARKGRAY);
-	int hspace = (100 - textwidth("score")) / 2;
-	int vspace = (50 - textheight("score")) / 2;
+	int hspace = (250 - textwidth("2048")) / 2;
+	int vspace = (200 - textheight("2048")) / 2;
+	outtextxy(110 + hspace, 60 + vspace, "2048");
+	//»ı·ÖÀ¸
+	setfillcolor(RGB(189, 172, 162));
+
+	solidroundrect(340, 100, 340 + 100, 100 + 100, 10, 10);
+	setfillcolor(RGB(189, 172, 162));
+	solidroundrect(450, 100, 450 + 100, 100 + 100, 10, 10);
+	settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
+	setbkmode(TRANSPARENT);
+	settextcolor(DARKGRAY);
+	hspace = (100 - textwidth("best")) / 2;
+	vspace = (50 - textheight("best")) / 2;
+	outtextxy(450 + hspace, 100 + vspace, "best");
+	char bestStr[20];
+	sprintf(bestStr, "%d", bestscore);
+	hspace = (100 - textwidth(bestStr)) / 2;
+	vspace = (50 - textheight(bestStr)) / 2;
+	outtextxy(450 + hspace, 150 + vspace, bestStr);
+	//Ê±¼ä
+	hspace = (100 - textwidth("00:00:00")) / 2;
+	vspace = (100 - textheight("00:00:00")) / 2;
+	settextstyle(30, 0, "Î¢ÈíÑÅºÚ");
+	outtextxy(325 + hspace, 200 + vspace, "00:00:00");
+	//³·Ïú ÔİÍ£ ½áÊø
+
+	settextstyle(35, 0, "Î¢ÈíÑÅºÚ");
+	setbkmode(TRANSPARENT);
+	settextcolor(DARKGRAY);
+	outtextxy(310, 280, "¡û");
+	settextstyle(30, 0, "Î¢ÈíÑÅºÚ");
+	settextcolor(DARKGRAY);
+	outtextxy(350, 280, "||");
+	settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
+	outtextxy(380, 275, "¡Á");
+	settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
+	setbkmode(TRANSPARENT);
+	settextcolor(DARKGRAY);
+	hspace = (100 - textwidth("score")) / 2;
+	vspace = (50 - textheight("score")) / 2;
 	outtextxy(340 + hspace, 100 + vspace, "score");
 	char scoreStr[20];
 	sprintf(scoreStr, "%d", score);
@@ -210,70 +257,29 @@ void updatedisplay(int score, int board[4][4])
 			givecolor(board[i][j], i, j);
 		}
 	}
+	EndBatchDraw();
 }
 void gamemenu(int board[4][4])
 {
 	ExMessage msg = { 0 };
-	//æ’¤å›
+	//³·»Ø
 	int backStack[3][4][4];  
 	int backScores[3];       
 	int backSteps[3];        
-	int backTop = -1;        // -1è¡¨ç¤ºç©º
+	int backTop = -1;        // -1±íÊ¾¿Õ
 
-	//åˆ†æ•°ï¼Œæ­¥æ•°
+	//·ÖÊı£¬²½Êı
 	int score = 0;
 	int step = 0;
-	int bestscore = 0;
-	//èƒŒæ™¯å’Œæ£‹ç›˜
-	setbkcolor(RGB(254, 246, 238));
-	cleardevice();
-	setfillcolor(RGB(189, 172, 162));
-	solidroundrect(100, 320, 100 + 520, 320 + 520, 50, 50);
-	//2048æ–‡å­—
-	settextstyle(100, 0, "å¾®è½¯é›…é»‘");
-	setbkmode(TRANSPARENT);
-	settextcolor(DARKGRAY);
-	int hspace = (250 - textwidth("2048")) / 2;
-	int vspace = (200 - textheight("2048")) / 2;
-	outtextxy(110 + hspace, 60 + vspace, "2048");
-	//ç§¯åˆ†æ 
-	setfillcolor(RGB(189, 172, 162));
-
-	solidroundrect(340, 100, 340 + 100, 100 + 100, 10, 10);
-	setfillcolor(RGB(189, 172, 162));
-	solidroundrect(450, 100, 450 + 100, 100 + 100, 10, 10);
-	settextstyle(40, 0, "å¾®è½¯é›…é»‘");
-	setbkmode(TRANSPARENT);
-	settextcolor(DARKGRAY);
-	hspace = (100 - textwidth("best")) / 2;
-	vspace = (50 - textheight("best")) / 2;
-	outtextxy(450 + hspace, 100 + vspace, "best");
-	char bestStr[20];
-	sprintf(bestStr, "%d", bestscore);
-	hspace = (100 - textwidth(bestStr)) / 2;
-	vspace = (50 - textheight(bestStr)) / 2;
-	outtextxy(450 + hspace, 150 + vspace, bestStr);
-	//æ—¶é—´
-	hspace = (100 - textwidth("00:00:00")) / 2;
-	vspace = (100 - textheight("00:00:00")) / 2;
-	settextstyle(30, 0, "å¾®è½¯é›…é»‘");
-	outtextxy(325 + hspace, 200 + vspace, "00:00:00");
-	//æ’¤é”€ æš‚åœ ç»“æŸ
-
-	settextstyle(35, 0, "å¾®è½¯é›…é»‘");
-	setbkmode(TRANSPARENT);
-	settextcolor(DARKGRAY);
-	outtextxy(310, 280, "â†");
-	settextstyle(30, 0, "å¾®è½¯é›…é»‘");
-	settextcolor(DARKGRAY);
-	outtextxy(350, 280, "||");
-	settextstyle(40, 0, "å¾®è½¯é›…é»‘");
-	outtextxy(380, 275, "Ã—");
-	//ç»™æ ¼å­ä¸Šè‰²å’Œæ•°å­—æ˜¾ç¤º
+	
+	//¸ø¸ñ×ÓÉÏÉ«ºÍÊı×ÖÏÔÊ¾
+	//Éú²ú2¸öËæ»úÊı
+	generateRandomTile(board);
+	generateRandomTile(board);
 	updatedisplay(score, board);
 	int checktime = 1000000;
 	int check = 1;
-	while (1)//æ¸¸æˆç•Œé¢ç”¨
+	while (1)//ÓÎÏ·½çÃæÓÃ
 	{
 		if (checktime)
 		{
@@ -281,7 +287,7 @@ void gamemenu(int board[4][4])
 		}
 		else
 		{
-			printf("æ£€æŸ¥\n");
+			printf("¼ì²é\n");
 
 			checktime = 1000000;
 			check = 0;
@@ -295,7 +301,7 @@ void gamemenu(int board[4][4])
 				}
 			if (!check)
 			{
-				gameover(score, step);//ç»“æŸæ¸¸æˆ
+				gameover(score, step);//½áÊøÓÎÏ·
 			}
 		}
 		if (peekmessage(&msg, EX_KEY | EX_MOUSE))
@@ -327,12 +333,19 @@ void gamemenu(int board[4][4])
 					case VK_UP:
 						printf("up\n");
 						step++;
+						numberadd(board, score, DIR_LEFT);
+						generateRandomTile(board);
+
+
 						updatedisplay(score, board);
 
 						break;
 					case VK_DOWN:
 						printf("down\n");
 						step++;
+						numberadd(board, score, DIR_RIGHT);
+						generateRandomTile(board);
+
 						updatedisplay(score, board);
 
 
@@ -340,6 +353,9 @@ void gamemenu(int board[4][4])
 					case VK_LEFT:
 						printf("left\n");
 						step++;
+						numberadd(board, score, DIR_UP);
+						generateRandomTile(board);
+
 						updatedisplay(score, board);
 
 
@@ -347,6 +363,9 @@ void gamemenu(int board[4][4])
 					case VK_RIGHT:
 						printf("right\n");
 						step++;
+						numberadd(board, score, DIR_DOWN );
+						generateRandomTile(board);
+
 						updatedisplay(score, board);
 
 
@@ -360,36 +379,36 @@ void gamemenu(int board[4][4])
 			switch (msg.message)
 			{
 			case WM_LBUTTONDOWN:
-				if (button(310, 280, 340, 310, msg.x, msg.y))//æ’¤å›
+				if (button(310, 280, 340, 310, msg.x, msg.y))//³·»Ø
 				{
-					printf("æ‚”æ­¥\n", msg.x, msg.y);
-					if (backTop >= 0) // å¦‚æœæœ‰å†å²è®°å½•
+					printf("»Ú²½\n", msg.x, msg.y);
+					if (backTop >= 0) // Èç¹ûÓĞÀúÊ·¼ÇÂ¼
 					{
-						printf("æ‚”æ­¥\n");
+						printf("»Ú²½\n");
 
-						// æ¢å¤æ£‹ç›˜
+						// »Ö¸´ÆåÅÌ
 						memcpy(board, backStack[backTop], sizeof(int) * 4 * 4);
 						score = backScores[backTop];
 						step = backSteps[backTop];
 
-						// æ›´æ–°æ˜¾ç¤º
+						// ¸üĞÂÏÔÊ¾
 						updatedisplay(score, board);
-						backTop--; // æ ˆé¡¶æŒ‡é’ˆå›é€€
+						backTop--; // Õ»¶¥Ö¸Õë»ØÍË
 					}
 					else
 					{
-						printf("æ— æ³•æ‚”æ£‹ï¼Œæ²¡æœ‰å†å²è®°å½•\n");
+						printf("ÎŞ·¨»ÚÆå£¬Ã»ÓĞÀúÊ·¼ÇÂ¼\n");
 					}
 				}
-				if (button(340, 280, 370, 310, msg.x, msg.y))//æš‚åœ
+				if (button(340, 280, 370, 310, msg.x, msg.y))//ÔİÍ£
 				{
-					printf("æš‚åœ\n", msg.x, msg.y);
+					printf("ÔİÍ£\n", msg.x, msg.y);
 
 				}
-				if (button(370, 280, 400, 310, msg.x, msg.y))//æ”¾å¼ƒ
+				if (button(370, 280, 400, 310, msg.x, msg.y))//·ÅÆú
 				{
-					printf("æ”¾å¼ƒ\n", msg.x, msg.y);
-					gameover(score, step);//ç»“æŸæ¸¸æˆ
+					printf("·ÅÆú\n", msg.x, msg.y);
+					gameover(score, step);//½áÊøÓÎÏ·
 				}
 				printf("%d,%d\n", msg.x, msg.y);
 				break;
@@ -414,7 +433,7 @@ void startbuttonshow(int y, const char str[],int a,int b,int c)
 
 	setfillcolor(RGB(a, b, c));
 	solidrectangle(150, y, 150 + 420, y + 80);
-	settextstyle(60, 0, "å¾®è½¯é›…é»‘");
+	settextstyle(60, 0, "Î¢ÈíÑÅºÚ");
 	int hspace = (420 - textwidth(str)) / 2;
 	int vspace = (80 - textheight(str)) / 2;
 	outtextxy(150 + hspace, y + vspace, str);
@@ -424,7 +443,7 @@ void ruleshow(int y, const char str[])
 {
 	setfillcolor(RGB(189, 172, 162));
 	solidrectangle(50, y, 50 + 620, y + 80);
-	settextstyle(30, 0, "å¾®è½¯é›…é»‘");
+	settextstyle(30, 0, "Î¢ÈíÑÅºÚ");
 	int hspace = (520 - textwidth(str)) / 2;
 	int vspace = (80 - textheight(str)) / 2;
 	outtextxy(100 + hspace, y + vspace, str);
@@ -435,23 +454,23 @@ void secondmenu()
 
 	cleardevice();
 
-	ruleshow(100, "ç›®æ ‡ï¼šé€šè¿‡åˆå¹¶ç›¸åŒæ•°å­—çš„æ–¹å—ï¼Œæœ€ç»ˆå¾—åˆ°ä¸€ä¸ª2048çš„æ–¹å—ã€‚");
+	ruleshow(100, "Ä¿±ê£ºÍ¨¹ıºÏ²¢ÏàÍ¬Êı×ÖµÄ·½¿é£¬×îÖÕµÃµ½Ò»¸ö2048µÄ·½¿é¡£");
 
-	ruleshow(200, "ç§»åŠ¨è§„åˆ™ï¼šç©å®¶å¯ä»¥é€šè¿‡ä¸Šä¸‹å·¦å³å››ä¸ªæ–¹å‘æ»‘åŠ¨");
-	ruleshow(260, "æ‰€æœ‰æ•°å­—æ–¹å—ä¼šå‘æ»‘åŠ¨çš„æ–¹å‘é æ‹¢");
+	ruleshow(200, "ÒÆ¶¯¹æÔò£ºÍæ¼Ò¿ÉÒÔÍ¨¹ıÉÏÏÂ×óÓÒËÄ¸ö·½Ïò»¬¶¯");
+	ruleshow(260, "ËùÓĞÊı×Ö·½¿é»áÏò»¬¶¯µÄ·½Ïò¿¿Â£");
 
-	ruleshow(360, "åˆå¹¶è§„åˆ™ï¼šç›¸åŒæ•°å­—çš„æ–¹å—åœ¨é æ‹¢æ—¶ä¼šç›¸åŠ ");
-	ruleshow(420, "å¹¶ä¸”å½¢æˆä¸€ä¸ªæ–°çš„æ–¹å—ã€‚");
+	ruleshow(360, "ºÏ²¢¹æÔò£ºÏàÍ¬Êı×ÖµÄ·½¿éÔÚ¿¿Â£Ê±»áÏà¼Ó");
+	ruleshow(420, "²¢ÇÒĞÎ³ÉÒ»¸öĞÂµÄ·½¿é¡£");
 
 
-	ruleshow(520, "ç”Ÿæˆè§„åˆ™ï¼šæ¯æ¬¡æ»‘åŠ¨åï¼Œç©ºç™½å¤„ä¼šéšæœºç”Ÿæˆä¸€ä¸ª1æˆ–2çš„æ–¹å—ã€‚");
-	ruleshow(620, "å¤±è´¥æ¡ä»¶ï¼šå¦‚æœæ‰€æœ‰æ ¼å­éƒ½å¡«æ»¡ä¸”æ²¡æœ‰ç›¸é‚»çš„ç›¸åŒæ•°å­—æ–¹å—ã€‚");
+	ruleshow(520, "Éú³É¹æÔò£ºÃ¿´Î»¬¶¯ºó£¬¿Õ°×´¦»áËæ»úÉú³ÉÒ»¸ö1»ò2µÄ·½¿é¡£");
+	ruleshow(620, "Ê§°ÜÌõ¼ş£ºÈç¹ûËùÓĞ¸ñ×Ó¶¼ÌîÂúÇÒÃ»ÓĞÏàÁÚµÄÏàÍ¬Êı×Ö·½¿é¡£");
 	int check = 1;
 	while (check)
 	{
 		BeginBatchDraw();
 
-		buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "ç¡®  è®¤");
+		buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "È·  ÈÏ");
 		EndBatchDraw();
 
 		if (peekmessage(&msg, EX_MOUSE))
@@ -459,7 +478,7 @@ void secondmenu()
 			switch (msg.message)
 			{
 			case WM_LBUTTONDOWN:
-				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//ç¡®è®¤
+				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//È·ÈÏ
 				{
 					check = 0;
 					startmenu();
@@ -476,56 +495,56 @@ void thirdmenu()
 {
 	ExMessage msg = { 0 };
 
-	//å·¦ä¾§ç¬¦å·
-	settextstyle(50, 0, "å¾®è½¯é›…é»‘");
+	//×ó²à·ûºÅ
+	settextstyle(50, 0, "Î¢ÈíÑÅºÚ");
 	setbkmode(TRANSPARENT);
 	settextcolor(DARKGRAY);
 	setfillcolor(RGB(189, 172, 162));
 	solidrectangle(150, 50, 150 + 150, 50 + 150);
-	outtextxy(215, 50, "â†‘");
-	outtextxy(215, 150, "â†“");
-	outtextxy(160, 100, "â†");
-	outtextxy(250, 100, "â†’");
+	outtextxy(215, 50, "¡ü");
+	outtextxy(215, 150, "¡ı");
+	outtextxy(160, 100, "¡û");
+	outtextxy(250, 100, "¡ú");
 	solidrectangle(150, 220, 150 + 150, 220 + 150);
-	int hspace = (150 - textwidth("â†")) / 2;
-	int vspace = (150 - textheight("â†")) / 2;
-	settextstyle(100, 0, "å¾®è½¯é›…é»‘");
-	outtextxy(130 + hspace, 200 + vspace, "â†");
+	int hspace = (150 - textwidth("¡û")) / 2;
+	int vspace = (150 - textheight("¡û")) / 2;
+	settextstyle(100, 0, "Î¢ÈíÑÅºÚ");
+	outtextxy(130 + hspace, 200 + vspace, "¡û");
 	solidrectangle(150, 390, 150 + 150, 390 + 150);
 	hspace = (150 - textwidth("||")) / 2;
 	vspace = (150 - textheight("||")) / 2;
 	outtextxy(150 + hspace, 390 + vspace, "||");
 	solidrectangle(150, 560, 150 + 150, 560 + 150);
-	hspace = (150 - textwidth("Ã—")) / 2;
-	vspace = (150 - textheight("Ã—")) / 2;
-	outtextxy(150 + hspace, 560 + vspace, "Ã—");
-	//å³ä¾§æ–‡å­—
-	settextstyle(40, 0, "å¾®è½¯é›…é»‘");
+	hspace = (150 - textwidth("¡Á")) / 2;
+	vspace = (150 - textheight("¡Á")) / 2;
+	outtextxy(150 + hspace, 560 + vspace, "¡Á");
+	//ÓÒ²àÎÄ×Ö
+	settextstyle(40, 0, "Î¢ÈíÑÅºÚ");
 
 	solidrectangle(350, 100, 350 + 250, 100 + 50);
-	hspace = (250 - textwidth("æŒ‰é”®ç›˜ä»¥ç§»åŠ¨æ–¹å—")) / 2;
-	vspace = (50 - textheight("æŒ‰é”®ç›˜ä»¥ç§»åŠ¨æ–¹å—")) / 2;
-	outtextxy(350 + hspace, 100 + vspace, "æŒ‰é”®ç›˜ä»¥ç§»åŠ¨æ–¹å—");
+	hspace = (250 - textwidth("°´¼üÅÌÒÔÒÆ¶¯·½¿é")) / 2;
+	vspace = (50 - textheight("°´¼üÅÌÒÔÒÆ¶¯·½¿é")) / 2;
+	outtextxy(350 + hspace, 100 + vspace, "°´¼üÅÌÒÔÒÆ¶¯·½¿é");
 	solidrectangle(350, 270, 350 + 250, 270 + 50);
-	hspace = (250 - textwidth("æ’¤å›ä¸€æ­¥")) / 2;
-	vspace = (50 - textheight("æ’¤å›ä¸€æ­¥")) / 2;
-	outtextxy(350 + hspace, 270 + vspace, "æ’¤å›ä¸€æ­¥");
+	hspace = (250 - textwidth("³·»ØÒ»²½")) / 2;
+	vspace = (50 - textheight("³·»ØÒ»²½")) / 2;
+	outtextxy(350 + hspace, 270 + vspace, "³·»ØÒ»²½");
 	solidrectangle(350, 440, 350 + 250, 440 + 50);
-	hspace = (250 - textwidth("æš‚åœè®¡æ—¶")) / 2;
-	vspace = (50 - textheight("æš‚åœè®¡æ—¶")) / 2;
-	outtextxy(350 + hspace, 440 + vspace, "æš‚åœè®¡æ—¶");
+	hspace = (250 - textwidth("ÔİÍ£¼ÆÊ±")) / 2;
+	vspace = (50 - textheight("ÔİÍ£¼ÆÊ±")) / 2;
+	outtextxy(350 + hspace, 440 + vspace, "ÔİÍ£¼ÆÊ±");
 	solidrectangle(350, 610, 350 + 250, 610 + 50);
-	hspace = (250 - textwidth("æ”¾å¼ƒæ¸¸æˆ")) / 2;
-	vspace = (50 - textheight("æ”¾å¼ƒæ¸¸æˆ")) / 2;
-	outtextxy(350 + hspace, 610 + vspace, "æ”¾å¼ƒæ¸¸æˆ");
-	//è¿”å›
+	hspace = (250 - textwidth("·ÅÆúÓÎÏ·")) / 2;
+	vspace = (50 - textheight("·ÅÆúÓÎÏ·")) / 2;
+	outtextxy(350 + hspace, 610 + vspace, "·ÅÆúÓÎÏ·");
+	//·µ»Ø
 
 	int check = 1;
 	while (check)
 	{
 		BeginBatchDraw();
 
-		buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "ç¡®  è®¤");
+		buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "È·  ÈÏ");
 		EndBatchDraw();
 
 		if (peekmessage(&msg, EX_MOUSE))
@@ -533,7 +552,7 @@ void thirdmenu()
 			switch (msg.message)
 			{
 			case WM_LBUTTONDOWN:
-				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//ç¡®è®¤
+				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//È·ÈÏ
 				{
 					check = 0;
 					startmenu();
@@ -550,32 +569,32 @@ void startmenu()
 {
 	ExMessage msg = { 0 };
 
-	//èƒŒæ™¯
+	//±³¾°
 	setbkcolor(RGB(254, 246, 238));
 	cleardevice();
 
-	//2048æ–‡å­—
-	settextstyle(200, 0, "å¾®è½¯é›…é»‘");
+	//2048ÎÄ×Ö
+	settextstyle(200, 0, "Î¢ÈíÑÅºÚ");
 	setbkmode(TRANSPARENT);
 	settextcolor(DARKGRAY);
 	int hspace = (320 - textwidth("2048")) / 2;
 	int vspace = (200 - textheight("2048")) / 2;
 	outtextxy(200 + hspace, vspace, "2048");
 	
-	//æŒ‰é”®
+	//°´¼ü
 	int check = 1;
 	while (check)
 	{
 		if (peekmessage(&msg, EX_MOUSE))
 		{
-			//æŒ‰é’®
+			//°´Å¥
 			BeginBatchDraw();
 
-			buttoncolor(150, 230, 150 + 420, 230 + 80, msg.x, msg.y, "å¼€ å§‹ æ¸¸ æˆ");
-			buttoncolor(150, 360, 150 + 420, 360 + 80, msg.x, msg.y, "æ¸¸ æˆ è§„ åˆ™");
-			buttoncolor(150, 490, 150 + 420, 490 + 80, msg.x, msg.y, "æŒ‰ é”® è¯´ æ˜");
-			buttoncolor(150, 620, 150 + 420, 620 + 80, msg.x, msg.y, "ç§¯ åˆ† æ’ å");
-			buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "é€€ å‡º æ¸¸ æˆ");
+			buttoncolor(150, 230, 150 + 420, 230 + 80, msg.x, msg.y, "¿ª Ê¼ ÓÎ Ï·");
+			buttoncolor(150, 360, 150 + 420, 360 + 80, msg.x, msg.y, "ÓÎ Ï· ¹æ Ôò");
+			buttoncolor(150, 490, 150 + 420, 490 + 80, msg.x, msg.y, "°´ ¼ü Ëµ Ã÷");
+			buttoncolor(150, 620, 150 + 420, 620 + 80, msg.x, msg.y, "»ı ·Ö ÅÅ Ãû");
+			buttoncolor(150, 750, 150 + 420, 750 + 80, msg.x, msg.y, "ÍË ³ö ÓÎ Ï·");
 			EndBatchDraw();
 
 
@@ -584,9 +603,9 @@ void startmenu()
 			switch (msg.message)
 			{
 			case WM_LBUTTONDOWN:
-				if (button(150, 230, 150 + 420, 230 + 80, msg.x, msg.y))//å¼€å§‹æ¸¸æˆ
+				if (button(150, 230, 150 + 420, 230 + 80, msg.x, msg.y))//¿ªÊ¼ÓÎÏ·
 				{
-					printf("å¼€å§‹æ¸¸æˆ\n", msg.x, msg.y);
+					printf("¿ªÊ¼ÓÎÏ·\n", msg.x, msg.y);
 					int board[row][col];
 					for (int i = 0;i < row;i++)
 					{
@@ -598,26 +617,26 @@ void startmenu()
 					gamemenu(board);
 
 				}
-				if (button(150, 360, 150 + 420, 360 + 80, msg.x, msg.y))//æ¸¸æˆè§„åˆ™
+				if (button(150, 360, 150 + 420, 360 + 80, msg.x, msg.y))//ÓÎÏ·¹æÔò
 				{
-					printf("æ¸¸æˆè§„åˆ™\n", msg.x, msg.y);
+					printf("ÓÎÏ·¹æÔò\n", msg.x, msg.y);
 					secondmenu();
 
 				}
-				if (button(150, 490, 150 + 420, 490 + 80, msg.x, msg.y))//æŒ‰é”®è¯´æ˜
+				if (button(150, 490, 150 + 420, 490 + 80, msg.x, msg.y))//°´¼üËµÃ÷
 				{
-					printf("æŒ‰é”®è¯´æ˜\n", msg.x, msg.y);
+					printf("°´¼üËµÃ÷\n", msg.x, msg.y);
 					cleardevice();
 					thirdmenu();
 				}
-				if (button(150, 620, 150 + 420, 620 + 80, msg.x, msg.y))//ç§¯åˆ†æ’å
+				if (button(150, 620, 150 + 420, 620 + 80, msg.x, msg.y))//»ı·ÖÅÅÃû
 				{
-					printf("ç§¯åˆ†æ’å\n", msg.x, msg.y);
+					printf("»ı·ÖÅÅÃû\n", msg.x, msg.y);
 
 				}
-				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//å…³é—­æ¸¸æˆ
+				if (button(150, 750, 150 + 420, 750 + 80, msg.x, msg.y))//¹Ø±ÕÓÎÏ·
 				{
-					printf("å…³é—­æ¸¸æˆ\n", msg.x, msg.y);
+					printf("¹Ø±ÕÓÎÏ·\n", msg.x, msg.y);
 					closegraph();
 					exit(0);
 				}
---
+++ run.cpp
@@ -0,0 +1,117 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "head.h"
+#include <iostream>
+void generateRandomTile(int board[4][4]) {
+    int emptyCells = 0;
+    int emptyPositions[16][2]; // 存储所有空白格子的坐标
+
+    // 收集所有空白格子
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (board[i][j] == 0) {
+                emptyPositions[emptyCells][0] = i;
+                emptyPositions[emptyCells][1] = j;
+                emptyCells++;
+            }
+        }
+    }
+
+    // 如果有空白格子，随机选择一个位置放置2或4
+    if (emptyCells > 0) {
+        int pos = rand() % emptyCells;
+        int value = (rand() % 10 == 0) ? 4 : 2; // 10%概率生成4
+        board[emptyPositions[pos][0]][emptyPositions[pos][1]] = value;
+    }
+}
+bool slideAndMerge(int line[4], int& score) {
+    bool moved = false;
+    int temp[4] = { 0 };
+    int index = 0;
+
+    // 1. 移动所有数字到左侧（去除0）
+    for (int i = 0; i < 4; i++) {
+        if (line[i] != 0) {
+            temp[index++] = line[i];
+        }
+    }
+
+    // 2. 合并相邻相同数字
+    for (int i = 0; i < 3; i++) {
+        if (temp[i] != 0 && temp[i] == temp[i + 1]) {
+            temp[i] *= 2;
+            score += temp[i]; // 加分
+            temp[i + 1] = 0;
+            moved = true;
+        }
+    }
+
+    // 3. 再次移动（处理合并后可能出现的空位）
+    index = 0;
+    for (int i = 0; i < 4; i++) {
+        line[i] = 0;
+        if (temp[i] != 0) {
+            line[index++] = temp[i];
+        }
+    }
+
+    // 检查是否发生移动（与原始数据比较）
+    for (int i = 0; i < 4; i++) {
+        if (line[i] != temp[i]) {
+            moved = true;
+        }
+    }
+
+    return moved;
+}
+
+// 主逻辑函数
+void numberadd(int board[4][4], int& score, int direction) {
+    bool moved = false;
+    int temp[4];
+
+    switch (direction) {
+    case DIR_UP:
+        // 处理每一列（从上到下）
+        for (int j = 0; j < 4; j++) {
+            // 提取一列
+            for (int i = 0; i < 4; i++) temp[i] = board[i][j];
+            // 处理这一列
+            if (slideAndMerge(temp, score)) moved = true;
+            // 写回
+            for (int i = 0; i < 4; i++) board[i][j] = temp[i];
+        }
+        break;
+
+    case DIR_DOWN:
+        // 处理每一列（从下到上）
+        for (int j = 0; j < 4; j++) {
+            // 提取一列（反向）
+            for (int i = 0; i < 4; i++) temp[3 - i] = board[i][j];
+            // 处理这一列
+            if (slideAndMerge(temp, score)) moved = true;
+            // 写回（反向）
+            for (int i = 0; i < 4; i++) board[i][j] = temp[3 - i];
+        }
+        break;
+
+    case DIR_LEFT:
+        // 处理每一行（从左到右）
+        for (int i = 0; i < 4; i++) {
+            // 直接处理行
+            if (slideAndMerge(board[i], score)) moved = true;
+        }
+        break;
+
+    case DIR_RIGHT:
+        // 处理每一行（从右到左）
+        for (int i = 0; i < 4; i++) {
+            // 提取一行（反向）
+            for (int j = 0; j < 4; j++) temp[3 - j] = board[i][j];
+            // 处理这一行
+            if (slideAndMerge(temp, score)) moved = true;
+            // 写回（反向）
+            for (int j = 0; j < 4; j++) board[i][j] = temp[3 - j];
+        }
+        break;
+    }
+}

159b145deb84ba76f4e163ac03384f70abf5c9e8
szxzip <venti@mondstadt.teyvat>
2025-05-30 14:28:27 +0800

    0

--- 2048.cpp
+++ 2048.cpp
@@ -1,23 +1,22 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "head.h"
+#include "header.h"
 #include <iostream>
 
 using namespace std;
-struct player
-{
-	string name;
-	int score;
-	int step;
-	int rank;
-	struct player* next = NULL;
+struct player {
+    string name;
+    int score;
+    int step;
+    int rank;
+    struct player* next = NULL;
 };
 int main()
 {
 
-	initgraph(720, 900);
-	setbkcolor(RGB(254, 246, 238));
-	cleardevice();
-	startmenu();
-	getchar();
-	return 0;
+    initgraph(720, 900);
+    setbkcolor(RGB(254, 246, 238));
+    cleardevice();
+    startmenu();
+    getchar();
+    return 0;
 }

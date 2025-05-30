a9e7d1f4c705f78a718509b8aa3e7c352c996fd4
wosikeaixiaonanniang <3146715671@qq.com>
2025-05-30 15:12:03 +0800

    Add files via upload

--- header.h
+++ header.h
@@ -1,26 +1,36 @@
 #pragma once
-#include <ctime>
-#include <easyx.h>
 #include <math.h>
 #include <string>
+#include <easyx.h>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+#include <chrono>
+#include <cmath>
+#include <easyx.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+using namespace std;
 
-const int row = 4;
-const int col = 4;
-const int score = 0;
-#define DIR_UP 1
-#define DIR_DOWN 2
-#define DIR_LEFT 3
+const int row = 4;//4行
+const int col = 4;//4列
+const int score = 0;//总得分
+#define DIR_UP    1
+#define DIR_DOWN  2
+#define DIR_LEFT  3
 #define DIR_RIGHT 4
-const int BOX_COUNT = 30;
+const int BOX_COUNT = 30; // 横框数量
+static string playername; // 横框数量
+
 
 struct Player {
     std::string name;
     int score;
     int step;
     int rank;
-    struct Player* next = NULL;
+    struct Player* next;
 };
-
 struct GameTime {
     int hours;
     int minutes;
@@ -34,9 +44,11 @@ struct GameTimer {
     bool isPaused;
 };
 
+void numberadd(int board[4][4], int& score, int direction);
+void generateRandomTile(int board[4][4]);
 void thirdmenu();
 bool button(int x1, int y1, int x2, int y2, int msgx, int msgy);
-void gamemenu(int board[4][4]);
+void gamemenu(int board[4][4],bool issignin);
 void givecolor(int num, int x, int y);
 void startbuttonshow(int y, const char str[], int a, int b, int c);
 void startmenu();
